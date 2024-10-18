27e2a8944568fe4f3d1d3b5c790e6193964aa9c8
liulillian1122 <110178@stu.hgsh.hc.edu.tw>
2024-10-18 11:34:29 +0800

    Update script.js

--- hw3/script.js
+++ hw3/script.js
@@ -1,16 +1,19 @@
 // 定義變數
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-let c = ["#743A3A","#B87070","#EBD6D6"];
-let c2= ["#3D7878","#81C0C0","#C4E1E1"];
-let c3= ["#5151A2","#7373B9","#9999CC"];
-let id=0,  x = 0, y = 0, dx = 5, dy = 5, r = 50, color = c[0];
-let id2=0, x2= canvas.width, y2= 0, dx2= 5, dy2= 5, r2= 10, color2= c2[0];
-let id3=0, x3= canvas.width/2, y3= 0, dx3= 5, dy3= 5, r3= 30, color3= c3[0];
+let c=[["#743A3A","#B87070","#EBD6D6"],
+		["#3D7878","#81C0C0","#C4E1E1"],
+		["#5151A2","#7373B9","#9999CC"]];
+
+let N=3, id=[0,0,0], r=[50,30,20];
+//let dx=[10,10,10], dy=[10,10,10];
+let dx=[20,20,20], dy=[20,20,20];
+
+let x=[r[0],canvas.width-r[1],canvas.width/2], y=[r[0],r[1],r[2]];
+let color=[ c[0][0], c[1][0], c[2][0] ];
 
 // 畫圓形
-function drawBall(x, y, r, color)
-{
+function drawBall(x, y, r, color){
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI * 2); // arc(圓心x, 圓心y, 半徑, 起始角, 結束角)
     ctx.fillStyle = color;
@@ -19,43 +22,47 @@ function drawBall(x, y, r, color)
 }
 
 // 更新畫布
-function draw()
-{
+function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    x+=dx,   y+=dy;
-	x2+=dx2, y2+=dy2;
-	x3+=dx3, y3+=dy3;
-	
+	for(let i=0; i<N; i++){
+		x[i]+=dx[i];
+		y[i]+=dy[i];
+	}		
     // TODO: 如果發生碰撞(畫布寬canvas.width, 畫布高canvas.height)，則改變速度(dx, dy)和顏色(color)
-    if(x<0||x>canvas.width){
-		dx=-dx;
-		id=(id+1)%3;
-		color = c[id];
+	for(let i=0; i<N; i++){
+		if(x[i]-r[i]<0||x[i]+r[i]>canvas.width){
+			dx[i]=-dx[i];
+			
+			if(x[i]-r[i]<0)	x[i] = r[i];
+			else			x[i] = canvas.width-r[i];
+			
+			id[i]=(id[i]+1)%3;
+			color[i] = c[i][id[i]];
+		}
+		if(y[i]-r[i]<0||y[i]+r[i]>canvas.height){
+			dy[i]=-dy[i];
+			
+			if(y[i]-r[i]<0)	y[i] = r[i];
+			else			y[i] = canvas.height-r[i];
+			id[i]=(id[i]+1)%3;
+			color[i] = c[i][id[i]];
+		}
 	}
-	if(y<0||y>canvas.height){
-		dy=-dy;
-		id=(id+1)%3;
-		color = c[id];
+	for(let i=0; i<N; i++){
+		for(let j=i+1; j<N; j++){
+			if((x[i]-x[j])*(x[i]-x[j]) + (y[i]-y[j])*(y[i]-y[j]) <= (r[i]+r[j])*(r[i]+r[j])){
+				let vcx=((r[i]*dx[i]+r[j]*dx[j])/(r[i]+r[j]));
+				let vcy=((r[i]*dy[i]+r[j]*dy[j])/(r[i]+r[j]));
+				dx[i]=2*vcx-dx[i];
+				dx[j]=2*vcx-dx[j];
+				dy[i]=2*vcy-dy[i];
+				dy[j]=2*vcy-dy[j];
+			}
+		}
 	}
-	if(x2<0||x2>canvas.width){
-		dx2=-dx2;
-		id2=(id2+1)%3;
-		color2 = c2[id2];
+	for(let i=0; i<N; i++){
+		drawBall(x[i], y[i], r[i], color[i]);
 	}
-	if(y2<0||y2>canvas.height){
-		dy2=-dy2;
-		id2=(id2+1)%3;
-		color2 = c2[id2];
-	}
-	if((x-x2)*(x-x2) + (y-y2)*(y-y2) < (r+r2)*(r+r2)){
-		let tx=(dx*(r-r2)+2*r2*dx2)/(r+r2);
-		let ty=(dy*(r-r2)+2*r2*dy2)/(r+r2);
-		dx2=(dx2*(r2-r)+2*r*dx)/(r2+r);
-		dy2=(dy2*(r2-r)+2*r*dy)/(r2+r);
-		[dx,dy]=[tx,ty];
-	}
-    drawBall(x, y, r, color);
-	drawBall(x2, y2, r2, color2);
     requestAnimationFrame(draw);
 }
 draw();
